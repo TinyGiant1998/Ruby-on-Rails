46f5748076bdd0d9e6c96504e80fb6239ac03df9
Suraj Budhathoki <tinysuraj143@gmail.com>
2024-11-21 20:11:53 +1100

    Added csv file for database track

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,13 @@
 class Article < ApplicationRecord
+after_save :update_csv # The callback method is added here
+private
+def update_csv
+  require "csv"
+  CSV.open("articles.csv", "wb") do |csv|
+    csv << Article.column_names
+    Article.all.each do |article|
+      csv << article.attributes.values
+    end
+  end
+end
 end
