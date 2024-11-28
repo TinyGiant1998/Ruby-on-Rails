ff24a0f2cd5992e1d014a52b299b263bd7244786
Suraj Budhathoki <tinysuraj143@gmail.com>
2024-11-28 15:54:23 +1100

    changes

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,11 +1,16 @@
-import "@hotwired/turbo-rails";
+// import { Application } from "@hotwired/stimulus";
+// import "@hotwired/turbo-rails";
 import "controllers";
-import { Turbo } from "@hotwired/turbo-rails";
+// import { Turbo } from "@hotwired/turbo-rails";
 
+const application = Application.start();
 // const application = Application.start();
 
 // Configure Stimulus development experience
+application.debug = false;
+window.Stimulus = application;
 // application.debug = false;
 // window.Stimulus = application;
 
+export { application };
 // export { application };
